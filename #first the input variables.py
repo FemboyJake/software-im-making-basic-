d9712cdbed4cc825ab1485a57c5268170105e909
FemboyJake <82147219+FemboyJake@users.noreply.github.com>
2022-07-24 23:19:24 +0100

    Add files via upload

---
+++ #first the input variables.py
@@ -0,0 +1,18 @@
+#first the input variables
+width=input("what is your width")
+length=input("what is your length")
+
+float(width)
+float(length)
+
+#next the output variables
+area=(float(width)*float(length))
+length2=(float(length)*2)
+width2=(float(width)*2)
+float(length2)
+float(width2)
+perimiter=(float(width2)+float(length2))
+
+#getting the variables to show
+print("your area is",area),
+print("your perimiter is",perimiter)
